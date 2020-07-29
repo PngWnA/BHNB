7a68b64a61332e4c175e052d4fd02ccb971d9351
PngWnA <2016320146@korea.ac.kr>
2020-07-30 02:17:54 +0900

    [Misc] Changed Test case

--- src/debug.js
+++ src/debug.js
@@ -3,6 +3,6 @@ init()
 .then(async () => this.geo = await getLocalGeographic())
 .then(async () => this.LST = getLocalSidereal(this.geo.lon))
 .then(async () => convertAllEquatorial())
-.then(async () => await(project(`GroundMap`)))
+.then(async () => await(project(`CelestialMap`)))
 .then(async () => console.log(`Done`))
 .catch(err => console.log(err));
