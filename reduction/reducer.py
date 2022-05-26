4a6f3c24e1ced5ec3f80cbf65811a87b451a43c0
PngWnA <gnu@korea.ac.kr>
2022-05-27 07:20:01 +0900

    [Reducer] Handles if mag is not declared

--- reduction/reducer.py
+++ reduction/reducer.py
@@ -1,23 +1,26 @@
-# Data format conversion
+# Data format
 import csv, json
 
 # Using argv
 import sys
 
 # argv check
-if not sys.argv[1]:
-    print("[-] Usage : python reducer.py {mag}")
+if len(sys.argv) < 2:
+    mag_min = 7.9
+else:
+    mag_min = float(sys.argv[1])
 
-# 어차피 파일 열면 GC가 알아서 꺼줄거임 ^^
 src = open("hygdata_v3.csv", "r")
 rows = csv.reader(src)
 rows = list(rows)
-
-# Reduction
 payload = list()
+
+#Get index of each column
 indexes = [ra, dec, proper, mag] = [rows[0].index(attr) for attr in ["ra", "dec", "proper", "mag"]]
+
 for row in rows[1:]:
-    if float(row[mag]) <= float(sys.argv[1]):
+    if float(row[mag]) <= mag_min:
+        #reduced <= [('ra', '0.000000'), ('dec', '0.000000'), ('proper', 'Sol'), ('mag', '-26.700')]
         reduced = zip([rows[0][index] for index in indexes],[row[index] for index in indexes])
         payload.append(dict(reduced))
 
